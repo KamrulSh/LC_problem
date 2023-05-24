64c26b5f336c14a1d5f6c3eeb86c8bda5fd4069b
Kamrul Islam Shahin <44506464+KamrulSh@users.noreply.github.com>
2023-05-24 11:34:21 +0600

    Add files via upload

--- Ex_2. Print All Subsequence.py
+++ Ex_2. Print All Subsequence.py
@@ -11,11 +11,16 @@ def printAllSubsequence(string):
     for i in range(len(smallOutput)):
         result[k] = string[0]+smallOutput[i]
         k += 1
+    print(result, "==>", len(result))
     return result
 
 
 print(printAllSubsequence("abcd"))
 
 """
+['', 'd'] ==> 2
+['', 'd', 'c', 'cd'] ==> 4
+['', 'd', 'c', 'cd', 'b', 'bd', 'bc', 'bcd'] ==> 8
+['', 'd', 'c', 'cd', 'b', 'bd', 'bc', 'bcd', 'a', 'ad', 'ac', 'acd', 'ab', 'abd', 'abc', 'abcd'] ==> 16
 ['', 'd', 'c', 'cd', 'b', 'bd', 'bc', 'bcd', 'a', 'ad', 'ac', 'acd', 'ab', 'abd', 'abc', 'abcd']
 """
